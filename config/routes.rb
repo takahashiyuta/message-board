5f82f0c84d5d4e29ea93bf8058d7b735546d1f33
sharuruneko <sharuruneko@icloud.com>
2016-03-07 05:25:24 +0000

    #[issueの番号] [コメント]

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,10 @@ Rails.application.routes.draw do
 
   get 'messages/index'
 
+  get 'messages/index'
+
+  get 'messages/index'
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 

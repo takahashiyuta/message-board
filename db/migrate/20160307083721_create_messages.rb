c33c7e09ea1d8df06b7e1068000e591bfd4f78ef
sharuruneko <sharuruneko@icloud.com>
2016-03-09 19:18:11 +0000

    fix links

--- db/migrate/20160307083721_create_messages.rb
+++ db/migrate/20160307083721_create_messages.rb
@@ -1,10 +1,10 @@
 class CreateMessages < ActiveRecord::Migration
-  def change
-    create_table :messages do |t|
-      t.string :name
-      t.string :body
+    def change
+        create_table :messages do |t|
+            t.string :name
+            t.string :body
 
-      t.timestamps null: false
+            t.timestamps null: false
+        end
     end
-  end
 end
